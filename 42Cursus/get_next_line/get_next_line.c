a4f07e3fa655ba6eba389657da0d03eeac429d26
Tegik97 <mennymachi@gmail.com>
2024-02-19 18:11:53 +0100

    aiuto

--- 42Cursus/get_next_line/get_next_line.c
+++ 42Cursus/get_next_line/get_next_line.c
@@ -7,22 +7,33 @@ char	*get_next_line(int fd)
 {
 	static char	*str;
 	char		*line;
-	ssize_t 	buffer;
+	ssize_t		buffer;
 	size_t		i;
-	
+
 	if (BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!str)
 	{
 		str = ft_calloc(BUFFER_SIZE, sizeof(*str));
+		if (!str)
+		{
+			free (str);
+			return (NULL);
+		}
 		buffer = read(fd, str, BUFFER_SIZE);
 		if (buffer == -1)
 		{
 			printf("error");
+			free (str);
 			return (NULL);
 		}
 	}
 	line = ft_calloc(ft_linelen(str), sizeof(*line));
+	if (!line)
+	{
+		free (line);
+		return (NULL);
+	}
 	i = 0;
 	while (*str && *str != '\n')
 		line[i++] = *str++;
@@ -31,15 +42,16 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	int	fd;
+	int		fd;
 
 	fd = open("get_next_line.txt", O_RDONLY);
+	fd = STDIN_FILENO;
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	close(fd);
 	return (0);
-}
+}*/
