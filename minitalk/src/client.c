af856e8049fd2c93100e3e5c7b25f94908b096e5
Tegik97 <mennymachi@gmail.com>
2024-06-29 16:53:27 +0200

    tester done

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchiaram <mchiaram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 16:41:48 by mchiaram          #+#    #+#             */
+/*   Updated: 2024/06/29 16:42:17 by mchiaram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 static void	send_char(int s_pid, unsigned char c)
@@ -37,19 +49,40 @@ static void	send_str(int s_pid, char *str)
 		send_char(s_pid, str[i++]);
 }
 
-int	main(int argc, char *argv[])
+static void	check_args(int argc, char *argv[])
 {
-	int	s_pid;
+	int	i;
 
 	if (argc != 3)
 	{
 		ft_putendl_fd("Error\nUsage: ./client <Server PID> <string>", 2);
 		exit(1);
 	}
+	if (!argv[2][0])
+	{
+		ft_putendl_fd("Error\nEmpty string", 2);
+		exit(1);
+	}
+	i = 0;
+	while (argv[1][i])
+	{
+		if (!ft_isdigit(argv[1][i++]))
+		{
+			ft_putendl_fd("Error\nInvalid PID", 2);
+			exit(1);
+		}
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	s_pid;
+
+	check_args(argc, argv);
 	s_pid = ft_atoi(argv[1]);
 	if (s_pid <= 0)
 	{
-		ft_putendl_fd("Error\nPID not valid", 2);
+		ft_putendl_fd("Error\nInvalid PID", 2);
 		exit(1);
 	}
 	send_str(s_pid, argv[2]);
--- minitalk/src/client_bonus.c
+++ minitalk/src/client_bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchiaram <mchiaram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 16:41:44 by mchiaram          #+#    #+#             */
+/*   Updated: 2024/06/29 16:53:21 by mchiaram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 int	g_received = 0;
@@ -55,19 +67,40 @@ static void	send_str(int s_pid, char *str)
 	}
 }
 
-int	main(int argc, char *argv[])
+static void	check_args(int argc, char *argv[])
 {
-	int	s_pid;
+	int	i;
 
 	if (argc != 3)
 	{
 		ft_putendl_fd("Error\nUsage: ./client <Server PID> <string>", 2);
 		exit(1);
 	}
+	if (!argv[2][0])
+	{
+		ft_putendl_fd("Error\nEmpty string", 2);
+		exit(1);
+	}
+	i = 0;
+	while (argv[1][i])
+	{
+		if (!ft_isdigit(argv[1][i++]))
+		{
+			ft_putendl_fd("Error\nInvalid PID", 2);
+			exit(1);
+		}
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	s_pid;
+
+	check_args(argc, argv);
 	s_pid = ft_atoi(argv[1]);
 	if (s_pid <= 0)
 	{
-		ft_putendl_fd("Error\nPID not valid", 2);
+		ft_putendl_fd("Error\nInvalid PID", 2);
 		exit(1);
 	}
 	signal(SIGUSR2, sig_handler);
