3a10990c64389a181ab836a80b9c00520ab03d96
Tegik97 <mennymachi@gmail.com>
2024-10-18 10:27:37 +0200

    last push

--- Circle_4/philosophers/philo/src/philo_actions.c
+++ Circle_4/philosophers/philo/src/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: mchiaram <mchiaram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:03:20 by mchiaram          #+#    #+#             */
-/*   Updated: 2024/10/17 18:34:02 by mchiaram         ###   ########.fr       */
+/*   Updated: 2024/10/18 10:26:04 by mchiaram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,39 +126,3 @@ int	philo_eat(t_philo *p, size_t tstamp)
 	pthread_mutex_unlock(&p->fork->next->lock);
 	return (1);
 }
-
-// int	philo_eat(t_philo *p)
-// {
-// 	size_t	tstamp;
-
-// 	pthread_mutex_lock(&p->fork->lock);
-// 	if (check_death(p))
-// 	{
-// 		pthread_mutex_unlock(&p->fork->lock);
-// 		return (0);
-// 	}
-// 	tstamp = get_time() - p->cond->inittime;
-// 	printf("\033[36m%ldms philo %ld has taken a fork\033[0m\n", tstamp, p->id);
-// 	if (p->next->id != p->id)
-// 	{
-// 		pthread_mutex_lock(&p->fork->next->lock);
-// 		if (check_death(p))
-// 		{
-// 			pthread_mutex_unlock(&p->fork->lock);
-// 			pthread_mutex_unlock(&p->fork->next->lock);
-// 			return (0);
-// 		}
-// 		tstamp = get_time() - p->cond->inittime;
-// 		printf("\033[32m%ldms philo %ld is eating\033[0m\n", tstamp, p->id);
-// 		pthread_mutex_lock(&p->cond->lockmeal);
-// 		p->last_meal = get_time();
-// 		pthread_mutex_unlock(&p->cond->lockmeal);
-// 		pthread_mutex_lock(&p->cond->lockate);
-// 		p->ate++;
-// 		pthread_mutex_unlock(&p->cond->lockate);
-// 		usleep(p->cond->eattime * 1000);
-// 		pthread_mutex_unlock(&p->fork->next->lock);
-// 	}
-// 	pthread_mutex_unlock(&p->fork->lock);
-// 	return (1);
-// }
