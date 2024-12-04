00fb0df6d254405f46bced955e62f388b450ebe9
Tegik97 <mennymachi@gmail.com>
2024-12-04 12:08:56 +0100

    miniminimini

--- Circle_4/minishell2/src/input_management/t_token_fill.c
+++ Circle_4/minishell2/src/input_management/t_token_fill.c
@@ -1,14 +1,71 @@
 #include "minishell.h"
 
+static void	fill_struct(t_parse *data, t_token *tok, size_t ntok)
+{
+	t_token	*new_tok;
+	t_redir	*new_rd;
+	size_t	i;
+
+	new_tok = malloc(sizeof(t_token));
+	new_tok->value = malloc(sizeof(char *) * (ntok + 1));
+	new_rd = NULL;
+	i = 0;
+	while (data && data->type != T_PIPE && ntok > 0)
+	{
+		if (data && (data->type == T_GENERAL || data->type == T_COMMAND
+				|| data->type == T_QUOTE || data->type == T_DQUOTE))
+			new_tok->value[i++] = data->value;
+		else if (data && (data->type == T_RED_IN || data->type == T_RED_OUT
+				|| data->type == T_RED_APPEN || data->type == T_RED_APPEN))
+		{
+			new_rd = malloc(sizeof(t_redir));
+			new_rd->type = data->type;
+			new_rd->name = data->next->value;
+		}
+		data = data->next;
+	}
+	new_tok->value[i] = NULL;
+	new_tok->rd = new_rd;
+	tok->next = new_tok;
+}
+
+static void	first_element(t_parse *data, t_token *tok, size_t ntok)
+{
+	t_redir	*rd;
+	size_t	i;
+
+	tok->value = malloc(sizeof(char *) * (ntok + 1));
+	rd = NULL;
+	i = 0;
+	while (data && data->type != T_PIPE && ntok > 0)
+	{
+		if (data && (data->type == T_GENERAL || data->type == T_COMMAND
+				|| data->type == T_QUOTE || data->type == T_DQUOTE))
+			tok->value[i++] = data->value;
+		else if (data && (data->type == T_RED_IN || data->type == T_RED_OUT
+				|| data->type == T_RED_APPEN || data->type == T_DELIM))
+		{
+			rd = malloc(sizeof(t_redir));
+			rd->type = data->type;
+			rd->name = data->next->value;
+		}
+		data = data->next;
+	}
+	tok->rd = rd;
+	tok->value[i] = NULL;
+	tok->next = NULL;
+}
+
 static int	n_tokens(t_parse *data)
 {
 	size_t	ntok;
 
 	ntok = 0;
-	while (data && (data->type == T_GENERAL || data->type == T_COMMAND
-			|| data->type == T_QUOTE || data->type == T_DQUOTE))
+	while (data && data->type != T_PIPE)
 	{
-		ntok++;
+		if (data->type == T_GENERAL || data->type == T_COMMAND
+			|| data->type == T_QUOTE || data->type == T_DQUOTE)
+			ntok++;
 		data = data->next;
 	}
 	return (ntok);
@@ -18,14 +75,23 @@ void	fill_t_token(t_parse *data, t_token *tok)
 {
 	size_t	ntok;
 
+	ntok = n_tokens(data);
+	first_element(data, tok, ntok);
+	while (ntok-- > 0)
+		data = data->next;
+	while (data && (data->type != T_GENERAL && data->type != T_COMMAND
+			&& data->type != T_QUOTE && data->type != T_DQUOTE))
+		data = data->next;
 	while (data)
 	{
 		ntok = n_tokens(data);
+		fill_struct(data, tok, ntok);
 		while (ntok-- > 0)
 			data = data->next;
 		while (data && (data->type != T_GENERAL && data->type != T_COMMAND
 				&& data->type != T_QUOTE && data->type != T_DQUOTE))
 			data = data->next;
+		tok = tok->next;
+		tok->next = NULL;
 	}
-	tok = (void *) tok;
 }
--- Circle_4/minishell2/src/parsing/parse.c
+++ Circle_4/minishell2/src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: menny <menny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:00:12 by mchiaram          #+#    #+#             */
-/*   Updated: 2024/12/03 15:16:35 by menny            ###   ########.fr       */
+/*   Updated: 2024/12/04 11:55:53 by menny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,25 @@
 
 int	input_parse(char *input, t_parse *data, t_token *tok)
 {
+	size_t	i;
+	size_t	j;
+
 	fill_t_parse_values(input, data);
 	fill_t_parse_types(data);
 	fill_t_token(data, tok);
 	printf("\n");
-	while (data)
+	j = 0;
+	while (tok)
 	{
-		printf("data->value: %s\n", data->value);
-		printf("data->type: %d\n", data->type);
+		i = 0;
+		while (tok->value[i])
+			printf("tok%ld->value: %s\n", j, tok->value[i++]);
+		if (tok->rd)
+			printf("tok%ld->rd->type: %d\ntok%ld->rd->name: %s\n", j, tok->rd->type, j, tok->rd->name);
 		printf("\n");
-		data = data->next;
+		tok = tok->next;
+		j++;
 	}
-	free_all(data);
+	// free_all(data);
 	return (0);
 }
