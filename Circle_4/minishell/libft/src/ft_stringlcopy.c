e64b44a37c32bf69544e92bd8672ef2c8339f7f0
Tegik97 <mennymachi@gmail.com>
2025-02-06 15:30:42 +0100

    minicd

--- Circle_4/minishell/libft/src/ft_stringlcopy.c
+++ Circle_4/minishell/libft/src/ft_stringlcopy.c
@@ -6,7 +6,7 @@
 /*   By: mchiaram <mchiaram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 11:27:37 by mchiaram          #+#    #+#             */
-/*   Updated: 2024/06/21 11:27:38 by mchiaram         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:17:41 by mchiaram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ char	*ft_stringlcopy(char *dst, const char *src, size_t size)
 
 	if (!src || !size)
 		return (NULL);
-	dst = ft_calloc((ft_strlen(src) + 1), sizeof(char));
+	dst = ft_calloc((size + 1), sizeof(char));
 	offset = 0;
-	while (*(src + offset) != '\0' && offset < (size - 1))
+	while (*(src + offset) != '\0' && offset < (size))
 	{
 		*(dst + offset) = *(src + offset);
 		offset++;
 	}
-	*(dst + offset) = '\0';
 	return (dst);
 }
--- Circle_4/minishell2/src/input_management/avoidablefilebutnorminette.c
+++
@@ -1,9 +0,0 @@
-#include "minishell.h"
-
-void	threerowstoolong(t_parse *data, char **v, char **v_val, char *str)
-{
-	free (*(v));
-	free(*(v_val));
-	free (data->value);
-	data->value = ft_strdup(str);
-}
