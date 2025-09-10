7c3caa342d4dda4a57295dae57ad08717470db70
Tegik97 <mennymachi@gmail.com>
2025-09-10 18:45:35 +0200

    Implement Bureaucrat class with exception handling and testing
    - Added Bureaucrat class with private members for name and grade.
    - Implemented constructors, copy constructor, assignment operator, and destructor.
    - Added methods for getting name and grade, and for incrementing and decrementing grade with exception handling.
    - Created custom exceptions for grade limits and assignment restrictions.
    - Developed a Makefile for building the project with clean and test targets.
    - Implemented main function to test Bureaucrat functionality including construction, grade manipulation, and exception handling.

---
+++ Circle_6/cpp00/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,78 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	throw DeclarationNotAllowedException();
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	this->_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): _name(other.getName()), _grade(other.getGrade()) {}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+		throw AssignmentNotAllowedException();
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat () {}
+
+const std::string& Bureaucrat::getName() const
+{
+	return this->_name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return this->_grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade <= 1)
+		throw GradeTooHighException();
+
+	_grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (_grade >= 150)
+		throw GradeTooLowException();
+
+	_grade++;
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Grade is too low";
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high";
+}
+
+const char* Bureaucrat::DeclarationNotAllowedException::what() const throw()
+{
+	return "Declaration not allowed, name and grade needed";
+}
+
+const char* Bureaucrat::AssignmentNotAllowedException::what() const throw()
+{
+	return "Assignment not allowed, name is constant";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return os;
+}
