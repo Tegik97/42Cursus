e23c200ad8fb3553b20562ea916986ff2d62ef17
Tegik97 <mennymachi@gmail.com>
2025-09-23 17:10:53 +0200

    Start of module6 ex00

---
+++ Circle_6/cpp/module_6/ex00/hpp/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+class ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter& operator=(const ScalarConverter& other);
+		~ScalarConverter();
+		
+};
+
+#endif
