9999ccf3d272b7c7c760bbdf679c38acc6675f4b
Tegik97 <mennymachi@gmail.com>
2025-10-21 15:38:42 +0200

    temp

--- Circle_6/cpp/module_9/ex00/hpp/BitcoinExchange.hpp
+++ Circle_6/cpp/module_9/ex00/hpp/BitcoinExchange.hpp
@@ -1,16 +1,20 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <stdexcept>
+# include <string>
+# include <vector>
+
 class BitcoinExchange
 {
 	private:
 		const std::string	_filename;
+		BitcoinExchange&	operator=(const BitcoinExchange& other);
 
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const std::string& filename);
 		BitcoinExchange(const BitcoinExchange& other);
-		BitcoinExchange&	operator=(const BitcoinExchange& other);
 		~BitcoinExchange();
 };
 
--- Circle_6/cpp/module_9/ex00/src/BitcoinExchange.cpp
+++ Circle_6/cpp/module_9/ex00/src/BitcoinExchange.cpp
@@ -2,10 +2,18 @@
 
 BitcoinExchange::BitcoinExchange() {}
 
-BitcoinExchange::BitcoinExchange(const std::string& filename) : _filename(filename) {}
+BitcoinExchange::BitcoinExchange(const std::string& filename) : _filename(filename)
+{
+	if (filename.empty())
+		throw std::invalid_argument("Error: Missing argument");
+}
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _filename(other._filename) {}
 
-BitcoinExchange&	operator=(const BitcoinExchange& other) {}
+BitcoinExchange&	operator=(const BitcoinExchange& other)
+{
+	(void)other;
+	return *this;
+}
 
 BitcoinExchange::~BitcoinExchange() {}
