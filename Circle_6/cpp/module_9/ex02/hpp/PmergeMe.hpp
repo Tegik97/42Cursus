1fc8c16ef4facdbeac5d8c0a7713f647a7788d3d
Tegik97 <mennymachi@gmail.com>
2025-11-10 17:10:27 +0100

    feat: implemented PmergeMe class OCF

---
+++ Circle_6/cpp/module_9/ex02/hpp/PmergeMe.hpp
@@ -0,0 +1,20 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <vector>
+# include <deque>
+
+class PmergeMe
+{
+	private:
+		std::vector<int>	_vec;
+		std::deque<int>		_deq;
+
+	public:
+		PmergeMe();
+		PmergeMe(const PmergeMe& other);
+		PmergeMe&	operator=(const PmergeMe& other);
+		~PmergeMe();
+};
+
+#endif
